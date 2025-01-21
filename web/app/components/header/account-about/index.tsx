2c69b46a4834b24143d8c70f59315326051fedc5
Moussa <moussbosse@gmail.com>
2025-01-21 18:02:22 +0100

    Footer

--- web/app/components/header/account-about/index.tsx
+++ web/app/components/header/account-about/index.tsx
@@ -45,8 +45,8 @@ export default function AccountAbout({
                 IS_CE_EDITION
                   ? <Link href={'https://github.com/langgenius/dify/blob/main/LICENSE'} target='_blank' rel='noopener noreferrer'>Open Source License</Link>
                   : <>
-                    <Link href='https://dify.ai/privacy' target='_blank' rel='noopener noreferrer'>Privacy Policy</Link>,<span> </span>
-                    <Link href='https://dify.ai/terms' target='_blank' rel='noopener noreferrer'>Terms of Service</Link>
+                    <Link href='https://elevatesolutions.fr/politique-de-confidentialite/' target='_blank' rel='noopener noreferrer'>Privacy Policy</Link>,<span> </span>
+                    <Link href='https://elevatesolutions.fr' target='_blank' rel='noopener noreferrer'>Terms of Service</Link>
                   </>
               }
             </div>
--- web/app/signin/normalForm.tsx
+++ web/app/signin/normalForm.tsx
@@ -196,7 +196,7 @@ const NormalForm = () => {
             <Link
               className='system-xs-medium text-text-secondary hover:underline'
               target='_blank' rel='noopener noreferrer'
-              href='https://dify.ai/privacy'
+              href='https://elevatesolutions.fr/politique-de-confidentialite/'
             >{t('login.pp')}</Link>
           </div>
           {IS_CE_EDITION && <div className="w-hull block mt-2 system-xs-regular text-text-tertiary">
